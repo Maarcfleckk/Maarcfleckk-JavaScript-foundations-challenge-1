70f15998f05d36a86bac68d4db4071e7e83b3dd7
Marc Fleck <marcfleckrossell4@gmail.com>
2023-10-17 20:24:37 +0200

    finish exercise 1

---
+++ js/exercise-00.js
@@ -0,0 +1,21 @@
+/* 
+   Create a constant variable of each of the following primitive types:
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+
+   Log out each variable to the terminal at the end.
+*/
+const primitiveString = "John Doe";
+const primitiveNumber = 3.1415;
+const primitiveBoolean = false;
+const primitiveNull = null;
+const primitiveUndefined = undefined;
+
+console.log(primitiveString);
+console.log(primitiveNumber);
+console.log(primitiveBoolean);
+console.log(primitiveNull);
+console.log(primitiveUndefined);
